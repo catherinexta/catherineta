31a836875af394cc6c3580045759bcec940babb4
Catherine Ta <catherine.ta@simoncomputing.com>
2018-12-19 11:01:00 -0500

    add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,25 @@ import * as React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
+import About from './features/about/about';
 import Header from './common/header/header';
 import Home from './features/home/home';
+import NotFound from './common/routes/not-found';
+import Resume from './features/resume/resume';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header />
+const App = () => (
+  <div className="App">
+    <Header />
 
-      <main>
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </main>
-    </div>
-  );
-};
+    <main>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/resume" component={Resume} />
+        <Route component={NotFound} />
+      </Switch>
+    </main>
+  </div>
+);
 
 export default App;
