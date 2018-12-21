90ab478fb654948afab16f22014a649096565682
Catherine Ta <catherineta@Catherines-MacBook-Air.local>
2018-12-21 16:30:22 -0500

    add new imagee on home

--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -1,11 +1,27 @@
 import * as React from 'react';
 
-const Home: React.SFC = () => {
+import { WithStyles, withStyles, createStyles } from '@material-ui/core/styles';
+
+import night from '../../common/images/night.jpg';
+
+const height = window.innerHeight;
+// const width = window.innerWidth;
+
+const styles = () =>
+  createStyles({
+    image: {
+      maxWidth: (height * 2) / 3
+    }
+  });
+
+const Home: React.SFC<WithStyles<typeof styles>> = props => {
+  const { classes } = props;
+
   return (
     <div>
-      <p>Something here</p>
+      <img src={night} className={classes.image} />
     </div>
   );
 };
 
-export default Home;
+export default withStyles(styles)(Home);
