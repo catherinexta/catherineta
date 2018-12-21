36234b141facc05b00143cf793913d1d0cb6402e
Catherine Ta <catherine.ta@simoncomputing.com>
2018-12-21 12:34:35 -0500

    udpate personal content

--- src/features/about/about.tsx
+++ src/features/about/about.tsx
@@ -8,7 +8,8 @@ import roses from '../../common/images/roses.jpg';
 const styles = () =>
   createStyles({
     image: {
-      maxHeight: '30em'
+      width: '100%',
+      height: 'auto'
     }
   });
 
@@ -16,13 +17,25 @@ const About: React.SFC<WithStyles<typeof styles>> = props => {
   const { classes } = props;
 
   return (
-    <Grid container spacing={32}>
-      <Grid item>
+    <Grid container spacing={32} justify="center">
+      <Grid item xs={12} md={4}>
         <img src={roses} className={classes.image} />
       </Grid>
-      <Grid item>
-        <h1>hello, i'm catherine,</h1>
-        <p>some more text here about me?</p>
+      <Grid item xs={12} md={6}>
+        <h1>hi, i'm catherine.</h1>
+        <p>
+          I'm currently a software developer at Simon Computing in Alexandria,
+          Virginia. Though I often develop on the full stack, my passion lies in
+          creating simple, yet innovative user interfaces. I always strive to
+          expand my knowledge on front end development and user experience
+          design.
+        </p>
+
+        <p>
+          In my free time, I like to pursue creative outlets, such as drawing
+          and editing travel videos. I also enjoy watching films in theater - in
+          the last year, I've viewed least fifteen movies in theaters.
+        </p>
       </Grid>
     </Grid>
   );
